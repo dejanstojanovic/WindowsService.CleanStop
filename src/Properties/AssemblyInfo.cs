fd6f15c7e2fb50d000ae69655e9d511fd6729832
d.stojanovic@hotmail.com <d.stojanovic@hotmail.com>
2017-09-21 09:18:42 +0400

    Added service description to AssemblyInfo.cs

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WindowsService.CleanStop")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("This is a sample clean service stop example Windows service")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WindowsService.CleanStop")]
--- src/ServiceInstaller.cs
+++ src/ServiceInstaller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration.Install;
 using System.Linq;
+using System.Reflection;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,11 +18,13 @@ namespace WindowsService.CleanStop
 
             var processInstaller = new System.ServiceProcess.ServiceProcessInstaller();
             var serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+            var assemblyDescription = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false).OfType<AssemblyDescriptionAttribute>().FirstOrDefault();
 
             processInstaller.Account = ServiceAccount.LocalSystem;
             serviceInstaller.DisplayName = typeof(Service).FullName;
             serviceInstaller.StartType = ServiceStartMode.Manual;
             serviceInstaller.ServiceName = typeof(Service).FullName;
+            serviceInstaller.Description = assemblyDescription!=null ? assemblyDescription.Description : null;
 
             this.Installers.Add(processInstaller);
             this.Installers.Add(serviceInstaller);
